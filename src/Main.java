2c21ed81823240d5d854ad3b1fc972b6ce96ce4e
Manuz <168524071+ManuelZuccolo@users.noreply.github.com>
2025-10-14 23:30:42 +0200

    Inout & output

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main
 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        RSA rsa = new RSA();
 
+        System.out.println("Chiave pubblica (n, e): " + rsa.getN() + ", " + rsa.getE());
+
+        System.out.print("Numero da cifrare (< n): ");
+        BigInteger message = input.nextBigInteger();
+
+        BigInteger cipher = rsa.encrypt(message);
+        System.out.println("Messaggio cifrato: " + cipher);
+
+        BigInteger decrypted = rsa.decrypt(cipher);
+        System.out.println("Messaggio decifrato: " + decrypted);
+
+        input.close();
+    }
 }
